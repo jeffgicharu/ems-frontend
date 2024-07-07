17386d6383b29a0889b40f5956c6b47ac43a1295
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2024-07-07 08:38:00 +0300

    Header and Footer Component

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,27 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { listEmployees } from '../services/EmployeeService';
 
 const ListEmployeeComponent = () => {
 
-    const dummyData = [
-        {
-            "id": 1,
-            "firstName": "Jeff",
-            "lastName": "Gicharu",
-            "email": "jeffgicharukk@gmail.com"
-        },
-        {
-            "id": 2,
-            "firstName": "Leehaney",
-            "lastName": "George",
-            "email": "lee@gmail.com"
-        },
-        {
-            "id": 3,
-            "firstName": "Linus",
-            "lastName": "Mwaniki",
-            "email": "linus@gmail.com"
-        }
-    ]
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(error=>{
+            console.error(error)
+        })
+    }, []);
 
     return (
         <div className='container'>
@@ -37,7 +27,7 @@ const ListEmployeeComponent = () => {
                 </thead>
                 <tbody>
                     {
-                        dummyData.map((employee) =>
+                        employees.map((employee) =>
                             <tr key={employee.id}>
                                 <td>{employee.id}</td>
                                 <td>{employee.firstName}</td>
